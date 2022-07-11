645c853139fd4c83e827bb3901d8f2a7d0fbe4b8
SAMBIT MAJHI <majhisambit2@gmail.com>
2022-07-11 10:36:47 +0530

    up

--- src/pages/hair.js
+++ src/pages/hair.js
@@ -24,15 +24,15 @@ function Hair() {
     setResponse(res.label === 1 ? "Combed": "Uncombed");
   };
     return (
-      <div><nav class="bg-gray-800">
-      <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-          <div class="relative flex items-center justify-between h-16">
-              <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                  <div class="hidden sm:block sm:ml-6">
-                      <div class="flex space-x-4">
-                          <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Clothes</a>
-                          <a href="/hair" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">hair</a>
-                          <a href="/weather" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Weather</a>
+      <div><nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="relative flex items-center justify-between h-16">
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                  <div className="hidden sm:block sm:ml-6">
+                      <div className="flex space-x-4">
+                          <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Clothes</a>
+                          <a href="/hair" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">hair</a>
+                          <a href="/weather" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Weather</a>
                       </div>
                   </div>
               </div>
--- src/pages/weather.js
+++ src/pages/weather.js
@@ -14,15 +14,15 @@ function Weather() {
   }, []);
   return (
     <div>{loader ? <p>Loading Data</p> : <>
-      <nav class="bg-gray-800">
-        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-          <div class="relative flex items-center justify-between h-16">
-            <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-              <div class="hidden sm:block sm:ml-6">
-                <div class="flex space-x-4">
-                  <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Clothes</a>
-                  <a href="/hair" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Hair</a>
-                  <a href="/weather" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Weather</a>
+      <nav className="bg-gray-800">
+        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="relative flex items-center justify-between h-16">
+            <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="hidden sm:block sm:ml-6">
+                <div className="flex space-x-4">
+                  <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Clothes</a>
+                  <a href="/hair" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Hair</a>
+                  <a href="/weather" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Weather</a>
                 </div>
               </div>
             </div>
